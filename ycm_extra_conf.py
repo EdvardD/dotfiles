d8614dae66217bb22697669d39d3a65a3ee7a1fb
Edvard Davtyan <davtyaneg@gmail.com>
2022-02-17 09:20:39 +0000

    Update ycm_extra_conf and tmux_conf

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -1,6 +1,8 @@
 import os
 import ycm_core
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
 flags = [
   '-Wall',
   '-Wextra',
@@ -9,12 +11,16 @@ flags = [
   'c++',
   '-isystem', '/usr/include/c++/7.4.0',
   '-I',
-  '.',
+  script_dir,
+  '-I',
+  script_dir + '/externals/include',
+  '-I',
+  script_dir + '/build/_deps/simdjson-src/include',
   ]
 
 SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', ]
 
-def FlagsForFile( filename, **kwargs ):
+def Settings( filename, **kwargs ):
   return {
   'flags': flags,
   'do_cache': True
