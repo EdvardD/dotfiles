2a8b3a9e572813a796586ad11ea769b938418d1c
Edvard Davtyan <davtyaneg@gmail.com>
2022-07-27 04:43:42 +0000

    Update install.sh for ubuntu 22.04

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ os_name=`uname`
 echo Install cmake, vim, g++, ack and ruby
 if [[ "$os_name" == "Linux" ]]; then
   sudo apt-get install -y software-properties-common
-  sudo add-apt-repository -y ppa:longsleep/golang-backports
+  if [[ "$(lsb_release -d)" == *"18.04"* ]]; then
+    sudo add-apt-repository -y ppa:longsleep/golang-backports
+  fi
   sudo apt-get update
   sudo apt-get --assume-yes install cmake vim g++-8 ack-grep ruby-dev golang-go python3 python3-pip curl tmux gdb htop tig
 else
@@ -19,13 +21,15 @@ echo Install Vundle
 rm -rf ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# echo Install YCM
-# rm -rf ~/.vim/bundle/YouCompleteMe
-# git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-# cd ~/.vim/bundle/YouCompleteMe
-# git submodule update --init --recursive
-# python3 install.py --clang-completer --gocode-completer --rust-completer
-# cd $DIR/dotfiles
+if [ "$1" = "--ycm" ] ; then
+  echo Install YCM
+  rm -rf ~/.vim/bundle/YouCompleteMe
+  git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+  cd ~/.vim/bundle/YouCompleteMe
+  git submodule update --init --recursive
+  python3 install.py --clang-completer --gocode-completer --rust-completer
+  cd $DIR/dotfiles
+fi
 
 echo Install dotfiles
 cp bashrc ~/.bashrc
